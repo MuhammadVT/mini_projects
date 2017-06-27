c4697520636edee1b1b15213eb4b983688d7fd96
MuhammadVT <mamatripik@gmail.com>
2017-06-27 14:14:03 -0400

    update __init__.py

--- remove_seasonal_noise/__init__.py
+++ remove_seasonal_noise/__init__.py
@@ -9,3 +9,8 @@ try:
     from moving_avg import moving_average
 except Exception, e:
     logging.exception(e)
+
+try:
+    from remove_high_freq import remove_high_freq_noise
+except Exception, e:
+    logging.exception(e)
